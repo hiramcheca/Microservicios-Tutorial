8c8c480bb859fd6b3c04029003c3b548c8c56212
Hiram Checa <Hiram.Checa@se.com>
2022-09-02 19:20:12 -0500

    gateway

--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -3,11 +3,15 @@ package com.usuario.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import com.usuario.service.configuration.LoadBalancerConfiguration;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
+@LoadBalancerClient(name = "usuario-service", configuration = LoadBalancerConfiguration.class)
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
---
+++ usuario-service/src/main/java/com/usuario/service/configuration/LoadBalancerConfiguration.java
@@ -0,0 +1,22 @@
+package com.usuario.service.configuration;
+
+
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadBalancerConfiguration {
+	
+	@Bean
+	   public ServiceInstanceListSupplier
+	discoveryClientServiceInstanceListSupplier(
+	         ConfigurableApplicationContext context) {
+	      System.out.println("Configurando Load balancer para las instancias");
+	      return ServiceInstanceListSupplier.builder()
+	               .withBlockingDiscoveryClient()
+	               .withSameInstancePreference()
+	               .build(context);
+	      }
+}
